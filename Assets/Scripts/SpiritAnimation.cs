db5800398ae6ce3e2324bc304c8c07ab0caa8a44
IdanS <idans2468@gmail.com>
2021-01-07 15:17:24 +0200

    update temp animations
    update temp animations

--- Assets/Scripts/SpiritAnimation.cs
+++ Assets/Scripts/SpiritAnimation.cs
@@ -12,16 +12,20 @@ public class SpiritAnimation : MonoBehaviour
     public float strength = 2f;
     private Sequence animation;
     private UIManger _uiManger;
+    private float intervalTime;
+    [Range(1,5)]
+    private float intervalRandomMin = 1f;
+    private float intervalRandomMax = 5f;
     // Start is called before the first frame update
     void Start()
     {
         _uiManger = FindObjectOfType<UIManger>();
         animation = DOTween.Sequence();
-        animation.Append(gameObject.transform.DOShakePosition(duration, strength));
-        animation.AppendInterval(1f);
-        animation.SetLoops(-1);
+        animation.Append(gameObject.transform.DOShakeScale(duration, strength));
+        animation.Join(gameObject.transform.DOShakePosition(duration, strength));
+        animation.AppendCallback(() => intervalTime = Random.Range(intervalRandomMin, intervalRandomMax));
+        animation.AppendInterval(intervalTime);
         animation.Play();
-
     }
 
     // Update is called once per frame
