3bc03ebb5125143fac97c370465f3ba8acc98da0
IdanS <idans2468@gmail.com>
2021-01-14 09:39:45 +0200

    Added "fly high" effect still not perfect
    Added "fly high" effect still not perfect

---
+++ Assets/FlyHighEffect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyHighEffect : MonoBehaviour
+{
+    [SerializeField] private float forceY = 10f;
+    [SerializeField] private Rigidbody2D rbPlayer;
+    [SerializeField] private float dragOverTime = 2f;
+    private float orgForceY;
+    private bool flyingUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other is EdgeCollider2D) return;
+        orgForceY = forceY;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other is EdgeCollider2D) return;
+        rbPlayer.AddForce(new Vector2(0,forceY));
+        forceY -= dragOverTime * Time.fixedDeltaTime;
+        if (forceY < 0)
+        {
+            forceY = orgForceY;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other is EdgeCollider2D) return;
+        forceY = orgForceY;
+    }
+}
