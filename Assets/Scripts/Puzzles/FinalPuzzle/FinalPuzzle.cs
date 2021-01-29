450314137c0779b61283ea3f478235db9ad8db41
CrumblingAway <62304010+CrumblingAway@users.noreply.github.com>
2021-01-29 21:46:10 +0200

    First draft of spot sequence puzzle
    - Built rough draft of last puzzle
    - Spots in puzzle have to be covered in certain order
    - Spots give no indication of progress
    - Sequence determinted by order in hierarchy

---
+++ Assets/Scripts/Puzzles/FinalPuzzle/FinalPuzzle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalPuzzle : MonoBehaviour
+{
+    [Header("Debugging")] [SerializeField] private SpotsPuzzle _spotsPuzzle;
+    [SerializeField] private GameObject _testObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spotsPuzzle = GetComponent<SpotsPuzzle>();
+        _spotsPuzzle.EventToTrigger = CrackSpiritRock;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CrackSpiritRock()
+    {
+        // TODO: CHANGE TO ACTUAL FUNCTION
+        _testObject.SetActive(false);
+    }
+}
